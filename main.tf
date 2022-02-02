18ab22c9dbf63ec2fec2f87965729b82d4b97922
manoj039 <84309456+manoj039@users.noreply.github.com>
2022-02-02 13:17:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "google_storage_bucket" "bucket" {
-  name = "test-bucket-random-001122"
+  name = "test-bucket-random-0012345"
   location = var.region
 }
 
 resource "google_storage_bucket" "gcs_bucket" {
-  name = "test-bucket-random-001123"
+  name = "test-bucket-random-0064224"
   location = var.region
 }
